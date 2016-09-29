f729b23cbfdb3662dc70ff617eecc5679a9d598d
Allan Oliveira <allanoliveira.main@gmail.com>
2016-09-28 21:05:58 -0300

    automigrate fix

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -5,7 +5,7 @@ var datasources = require(path.resolve(__dirname, '../datasources.json'));
 
 function autoMigrateAll(){
   Object.keys(models).forEach(function(key) {
-    if (typeof models[key].dataSource != 'undefined') {
+    if (typeof models[key].dataSource != 'undefined' && models[key].dataSource!='db') {
       if (typeof datasources[models[key].dataSource] != 'undefined') {
         app.dataSources[models[key].dataSource].automigrate(key, function (err) {
           if (err) throw err;
